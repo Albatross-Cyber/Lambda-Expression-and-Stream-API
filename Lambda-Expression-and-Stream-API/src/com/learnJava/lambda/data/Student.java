8abd8ad1a992f71b169e87f658c8637a1e14ad42
Albatross-Cyber <abhinavrastogi41@gmail.com>
2020-06-12 21:57:39 +0530

    Revert "Streams API"
    This reverts commit 6d5aa7afc20f4e25a6f492a55ba3910843748019.

--- Lambda-Expression-and-Stream-API/src/com/learnJava/lambda/data/Student.java
+++ Lambda-Expression-and-Stream-API/src/com/learnJava/lambda/data/Student.java
@@ -9,19 +9,8 @@ public class Student {
     private double gpa;
     private String gender;
     List<String> activities = new ArrayList<>();
-    private int noteBooks;
 
-    public Student(String name, int gradeLevel, double gpa, String gender, List<String> activities, int noteBooks) {
-		super();
-		this.name = name;
-		this.gradeLevel = gradeLevel;
-		this.gpa = gpa;
-		this.gender = gender;
-		this.activities = activities;
-		this.setNoteBooks(noteBooks);
-	}
-
-	public Student(String name, int gradeLevel, double gpa, String gender, List<String> activities) {
+    public Student(String name, int gradeLevel, double gpa, String gender, List<String> activities) {
         this.name = name;
         this.gradeLevel = gradeLevel;
         this.gpa = gpa;
@@ -92,13 +81,5 @@ public class Student {
 	
 	public Student(String s) {
 	    this.name = s;	
-	}
-
-	public int getNoteBooks() {
-		return noteBooks;
-	}
-
-	public void setNoteBooks(int noteBooks) {
-		this.noteBooks = noteBooks;
 	}
 }
